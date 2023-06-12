404cac167093dc6cacedbd6e61ddd1ddd034f9f2
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-06-13 00:47:32 +0100

    Add favicons

--- app/root.tsx
+++ app/root.tsx
@@ -8,12 +8,25 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
 import stylesheet from "~/styles.css";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png",
+  },
+  { rel: "manifest", href: "/site.webmanifest" },
 ];
 
 export default function App() {
