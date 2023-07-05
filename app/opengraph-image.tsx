2f6e980ec5e550d792f15b1a8d4e3e3937f8be04
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-07-05 13:30:20 +0100

    Post OG image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,9 +1,10 @@
 import { ImageResponse } from "next/server";
 
+// Route segment config
 export const runtime = "edge";
 
+// Image metadata
 export const alt = "Daniel Grant";
-
 export const size = {
   width: 1200,
   height: 630,
@@ -11,9 +12,17 @@ export const size = {
 
 export const contentType = "image/png";
 
+// Font
+const getPalatino = () =>
+  fetch(new URL("./Palatino.ttf", import.meta.url)).then((res) =>
+    res.arrayBuffer()
+  );
+
+// Image generation
 export default async function Image() {
   return new ImageResponse(
     (
+      // ImageResponse JSX element
       <div
         style={{
           fontSize: 128,
@@ -28,14 +37,15 @@ export default async function Image() {
         Daniel Grant
       </div>
     ),
+    // ImageResponse options
     {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
       ...size,
       fonts: [
         {
-          name: "Palatino",
-          data: await fetch(new URL("./Palatino.ttf", import.meta.url)).then(
-            (res) => res.arrayBuffer()
-          ),
+          name: "Inter",
+          data: await getPalatino(),
         },
       ],
     }
---
+++ utils/opengraph.ts
@@ -0,0 +1,15 @@
+export const ogSize = { width: 1200, height: 630 };
+
+export async function getOgImageOptions() {
+  return {
+    ...ogSize,
+    fonts: [
+      {
+        name: "Palatino",
+        data: await fetch(new URL("./Palatino.ttf", import.meta.url)).then(
+          (res) => res.arrayBuffer()
+        ),
+      },
+    ],
+  };
+}
