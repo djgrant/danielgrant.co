0e6fa75d89a2fdb0910cf647c2f0f677e1937e55
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-07-06 16:06:22 +0100

    Redirects posts to homepage

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  redirects() {
+    return [
+      {
+        source: "/posts",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
