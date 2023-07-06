ca6366c18bae5f61d62ee1af2773757ae3f4733f
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2023-07-06 16:15:34 +0100

    404 page for posts

--- models/posts.model.ts
+++ models/posts.model.ts
@@ -1,9 +1,12 @@
 import type { Page, PageMeta } from "notion-cms";
+import { notFound } from "next/navigation";
 
 export const getPosts = async () => {
-  return (await import("../data/posts/index.json")).default as PageMeta[];
+  return (await import("../data/posts/index.json").catch(notFound))
+    .default as PageMeta[];
 };
 
 export const getPost = async (slug: string) => {
-  return (await import(`../data/posts/${slug}.json`)).default as Page;
+  return (await import(`../data/posts/${slug}.json`).catch(notFound))
+    .default as Page;
 };
